764fee26984fb1ae8160bc6fb1b2ee5b3b9ffeda
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-04-01 20:39:59 +0530

    WIP: auth

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
+export default function Page() {
+  return (
+    <main>
+      Hello
+      <LoginLink>Sign in</LoginLink>
+    </main>
+  );
+}
