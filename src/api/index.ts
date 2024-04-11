65abe542340373428450196f007e64be36f51e7b
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-04-11 07:42:25 +0530

    chore: fix ky instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,6 @@
 import ky from "ky";
 
-/**
- * Base API instance, it is shared between client and server
- */
-export const baseApi = ky.extend({
+export const api = ky.extend({
   prefixUrl: process.env.BASE_URL,
-});
-
-/**
- * Client API instance, it is used only in client
- */
-export const clientApi = ky.extend({
-  prefixUrl: process.env.BASE_URL,
-  hooks: {
-    beforeRequest: [
-      async (req) => {
-        return req;
-      },
-    ],
-  },
+  retry: 1,
 });
