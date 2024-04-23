2014ea3c62c893b43afdc1c943c3689aeb71aae0
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-04-24 01:43:41 +0530

    chore: add fullscreen in skeleton

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,15 +1,20 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
+  fullscreen,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: React.HTMLAttributes<HTMLDivElement> & { fullscreen?: boolean }) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn(
+        "animate-pulse rounded-md bg-muted",
+        fullscreen && "min-h-[80vh]",
+        className
+      )}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
