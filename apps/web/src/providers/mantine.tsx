93f4c40f01b3da5fc79d65b415ab4c755c56b8d7
shareef-s7 <nadeemshareef934@gmail.com>
2026-05-31 18:18:31 +0530

    feat: integrate MantineProvider into routing structure and add button component

--- apps/web/src/providers/mantine.tsx
+++ apps/web/src/providers/mantine.tsx
@@ -15,7 +15,6 @@ import {
 import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/dates/styles.css";
-import "@mantine/dropzone/styles.css";
 import "@mantine/notifications/styles.css";
 import { Notifications } from "@mantine/notifications";
 
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -8,6 +9,9 @@ function RouteComponent() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold">Shareef Money</h1>
+      <Button className="ml-4" onClick={() => alert("Hello, world!")}>
+        Click me
+      </Button>
     </div>
   );
 }
